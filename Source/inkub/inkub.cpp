1ce536922fc391c4ae128efe4a495aba44310566
Kornel Mrozowski <kpmrozowski@gmail.com>
2022-07-03 20:05:11 +0200

    unreal engine 4 project initiated

---
+++ Source/inkub/inkub.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "inkub.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, inkub, "inkub" );
---
+++ Source/inkub/inkubGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "inkubGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INKUB_API AinkubGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
